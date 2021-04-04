3bb67df591bfa0aebf1c812dfed845fe4a1ce565
Evgeniy3171 <evgen915628@list.ru>
2021-04-04 18:50:11 +0300

    Поправил операцию возведения в степень

--- lesson2/hard_lesson2.js
+++ lesson2/hard_lesson2.js
@@ -5,5 +5,5 @@ for (let i = 0; i < a; i++) {
   resault = resault * String(num)[i];
 }
 console.log(resault);
-resault = resault * resault * resault;
+resault = resault ** 3;
 console.log(String(resault)[0] + String(resault)[1]);
