4f0e0dbaa0fbd867b46ca405adadf2787d7ecfda
Evgeniy3171 <evgen915628@list.ru>
2021-04-05 21:49:00 +0300

    Завершил выполнение усложненого задания к уроку № 3

--- lesson2/hard_lesson2.js
+++ lesson2/hard_lesson2.js
@@ -10,4 +10,41 @@ resault = resault ** 3;
 console.log(String(resault)[0] + String(resault)[1]); */
 
 /* Усложненное задание к уроку № 3 */
-let lang;
+/* let lang;
+let daysOfTheWeek = [
+  ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"],
+  ["monday", "tuesday", "wednesday", "thursday",	"friday", "saturday", "sunday"]
+];
+lang = prompt("Укажите, на каком языке будут отображаться дни недели", '"ru" или "en"');
+ */
+/* Через if */
+/* if (lang === "ru") {
+  alert("Дни недели: понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+} else {
+  alert("Days of the week: monday, tuesday, wednesday, thursday,	friday, saturday, sunday");
+}
+ */
+/* Через switch */
+/* switch (lang) {
+  case "ru":
+    alert("Дни недели: понедельник, вторник, среда, четверг, пятница, суббота, воскресенье");
+    break;
+  case "en":
+    alert("Days of the week: monday, tuesday, wednesday, thursday,	friday, saturday, sunday");
+    break;
+  default:
+    alert("Возникла ошибка");
+} */
+
+/* Через arr */
+/* if (lang === "ru") {
+  alert(daysOfTheWeek[0]);
+} else {
+  alert(daysOfTheWeek[1]);
+} */
+
+let namePerson;
+namePerson = prompt("Укажите имя пользователя");
+namePerson = 
+  (namePerson === "Артем") ? console.log("Директор") :
+    (namePerson === "Максим") ? console.log("Преподаватель") : console.log("Студент");
