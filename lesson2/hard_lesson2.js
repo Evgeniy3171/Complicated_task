c47fd804dcb14460f5a5494b335f7988f1996e87
Evgeniy3171 <evgen915628@list.ru>
2021-04-24 20:27:26 +0300

    Приступил к усложненнке к уроку 5

--- lesson2/hard_lesson2.js
+++ lesson2/hard_lesson2.js
@@ -1,13 +1,1 @@
 'use strict';
-
-let checkInput = function(n){
-  if (!isNaN(n) || n === ''){
-    checkInput(prompt('Вы ввели не строку. Введите строку.'));
-  };
-  n = n.trim();         //убираю пробелы лишние
-  if (n.length > 30){
-    n = n.slice(0, 30) + "..."; //после 30-го символа заменяю все остальные на ...
-    alert('Вы ввели '+n);
-  }
-};
-checkInput(prompt('Введите строку.'));
